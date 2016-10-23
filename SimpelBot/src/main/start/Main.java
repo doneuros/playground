eb1d7af7270241fc3f411695c10b7edf64b74318
Marc <doneuros11@web.de>
2016-10-23 20:03:56 +0200

    Added simple mouse press Bot for idle games

--- SimpelBot/src/main/start/Main.java
+++ SimpelBot/src/main/start/Main.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.InputEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,21 +20,24 @@ import lc.kra.system.mouse.event.GlobalMouseEvent;
 
 public class Main {
 
-	public static void main(String[] args) {
-		InfoView iv = new InfoView();
-		
+	public static void main(String[] args) throws AWTException, InterruptedException {
+		//InfoView iv = new InfoView();
+		Thread.sleep(200);
+		Robot r = new Robot();
 		for (int i = 0; i < 1000; i++) {
 			try {
-				Thread.sleep(500);
+
+				r.mousePress(InputEvent.BUTTON1_MASK);
+				Thread.sleep(70);
+				r.mouseRelease(InputEvent.BUTTON1_MASK);
+				//Thread.sleep(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			iv.update();
+			//iv.update();
 		}
-		
-		
 		//imageEditing();
 	}
-	
-	
 }
+
+
